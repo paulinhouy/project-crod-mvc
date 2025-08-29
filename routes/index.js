116d039476fa5b635e5a621eeb568d8ff6585ff4
Paulo <paulofilhorocha22@gmail.com>
2025-08-29 19:06:41 -0300

    git

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,11 @@ router.post("/atendimento",(req,res) => {
     res.send('estamos criando o postS')
 })
 
-router.put("/atendimento",(req,res) => {
+router.put("/atendimento/:id",(req,res) => {
+    const {id} = req.params;
+    res.send(`put atualização ${id}`)
+})
+router.delete("/atendimento",(req,res) => {
+        const {id} = req.params;
     res.send('put atualização')
 })
