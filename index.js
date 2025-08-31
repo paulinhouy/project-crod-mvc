79ea4d6ae84f4ad6e8e06314f08092a2bb8e002a
Paulo <paulofilhorocha22@gmail.com>
2025-08-31 11:11:49 -0300

    git

--- index.js
+++ index.js
@@ -2,7 +2,33 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-app.listen(3000,(error) =>{
+const {Router} = require('express')
+const router = Router();
+
+//get post put delete
+
+router.get("/atendimento",(req,res) => {
+    res.send('entrei no  get')
+})
+
+router.post("/atendimento",(req,res) => {
+    res.send('estamos criando o postS')
+})
+
+router.put("/atendimento/:id",(req,res) => {
+    const {id} = req.params;
+    res.send(`put atualização ${id}`)
+})
+router.delete("/atendimento/:id",(req,res) => {
+        const {id} = req.params;
+    res.send(`delete id ${id}`)
+})
+
+
+
+app.use("/", router);
+
+app.listen(port,(error) =>{
 if(error){
     console.log('deu erro')
     return;
