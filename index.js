dd0b231613384fce842f27ee72d13d81936d20c3
Paulo <paulofilhorocha22@gmail.com>
2025-08-31 23:39:07 -0300

    git

--- index.js
+++ index.js
@@ -2,31 +2,7 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-const {Router} = require('express')
-const router = Router();
 
-//get post put delete
-
-router.get("/atendimento",(req,res) => {
-    res.send('entrei no  get')
-})
-
-router.post("/atendimento",(req,res) => {
-    res.send('estamos criando o postS')
-})
-
-router.put("/atendimento/:id",(req,res) => {
-    const {id} = req.params;
-    res.send(`put atualização ${id}`)
-})
-router.delete("/atendimento/:id",(req,res) => {
-        const {id} = req.params;
-    res.send(`delete id ${id}`)
-})
-
-
-
-app.use("/", router);
 
 app.listen(port,(error) =>{
 if(error){
