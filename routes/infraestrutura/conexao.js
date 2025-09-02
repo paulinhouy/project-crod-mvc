97233fc69b7ad0c24c5abfb1ffd245fbd500c718
Paulo <paulofilhorocha22@gmail.com>
2025-09-02 08:48:55 -0300

    conexao com mysql

--- routes/infraestrutura/conexao.js
+++ routes/infraestrutura/conexao.js
@@ -1,1 +1,12 @@
-const conexao = require('mysql')
+const mysql = require('mysql')
+
+const conexao = mysql.createConnection({
+
+    host: "localhost",
+    port: 3006,
+    user:"root",
+    password:"",
+    database:"atendimentos",
+})
+
+
