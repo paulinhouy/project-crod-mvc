7a69efbad867b1663649b7e0ab437fae8d9efc29
Paulo <paulofilhorocha22@gmail.com>
2025-09-16 19:53:55 -0300

    projeto finalizado,somente testar

--- routes/infraestrutura/conexao.js
+++ routes/infraestrutura/conexao.js
@@ -1,13 +1,19 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
 
 const conexao = mysql.createConnection({
-
     host: "localhost",
-    port:3306,
-    user:"root",
-    password:"",
-    database:"project_crod_mvc",
-})
-module.exports = conexao;
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "project_crod_mvc",
+});
 
+conexao.connect((error) => {
+    if (error) {
+        console.log("❌ Erro ao conectar no MySQL:", error);
+    } else {
+        console.log("✅ Conectado ao MySQL");
+    }
+});
 
+module.exports = conexao; // <- CORRETO
